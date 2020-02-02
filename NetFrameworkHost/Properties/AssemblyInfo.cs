fc5fe3f23c5498f0db2dccf0d1a62a6df1cf5ca3
Gratian <18526190+gpa@users.noreply.github.com>
2020-02-02 14:00:35 +0100

    Added NetFrameworkHost

---
+++ NetFrameworkHost/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("JiraIntegrationTool.NetFrameworkHost")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("JiraIntegrationTool.NetFrameworkHost")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("11a6a31b-628a-4200-b24e-bd4bcf3038b7")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
