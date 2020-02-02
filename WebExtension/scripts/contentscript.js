605168cc5957dfc20906bc44f593451dc231386f
Gratian <18526190+gpa@users.noreply.github.com>
2020-02-02 16:03:17 +0100

    Remove removing script after injecting

--- WebExtension/scripts/contentscript.js
+++ WebExtension/scripts/contentscript.js
@@ -1,8 +1,6 @@
 const script = document.createElement('script');
 script.src = browser.runtime.getURL('scripts/pagescript.js');
-script.onload = () => this.remove();
 (document.head || document.documentElement).appendChild(script);
-
 document.addEventListener('branchCheckoutTriggeredEvent', e => {
   browser.runtime.sendMessage({ method: 'checkoutBranch', params: e.detail });
 });
